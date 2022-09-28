b40fd29b1333ef803b86f4643365bcab7721946c
Darab-Khan-1 <darabkhanse@gmail.com>
2022-09-28 11:33:46 +0500

    code refactor

--- public/class-lti-platform-public.php
+++ public/class-lti-platform-public.php
@@ -66,55 +66,11 @@ class LTI_Platform_Public
         $this->version = $version;
     }
 
-    private function parse_custom_parameters($customparams)  
-    {
-        $custom = array();
-        $params = null;
-        if (!empty($customparams)) {
-            parse_str(str_replace('&#13;&#10;', '&', $customparams), $custom);
-            foreach ($custom as $name => $value) {
-                $name = preg_replace('/[^a-z0-9]/', '_', strtolower(trim($name)));
-                if (!empty($name)) {
-                    $params["custom_{$name}"] = $value;
-                }
-            }
-        }
-        return $params;
-    }
-
-    private function check_content_type($contenturl)    
-    {
-        if (str_contains($contenturl['path'], '/mod/curriki') && str_contains($contenturl['query'], 'activity=')) {
-            parse_str($contenturl['query'], $params);
-            $contentpublicurl = "/lti-tools/activity/" . $params['activity'];
-        }
-
-        return $contentpublicurl;
-    }
-
     public function parse_request()
     {
         if (isset($_GET[LTI_Platform::get_plugin_name()])) {
             if (!is_user_logged_in()) {                                              
-                $post = get_post(intval(sanitize_text_field($_GET['post'])));
-                $link_atts = $this->get_link_atts($post, sanitize_text_field($_GET['id']));
-                $contenturl = parse_url($link_atts['url']);
-                $contentpublicurl = $this->check_content_type($contenturl);
-                $tool = LTI_Platform_Tool::fromCode($link_atts['tool'], LTI_Platform::$ltiPlatformDataConnector);
-                $customparams = $this->parse_custom_parameters($tool->getSetting('custom'));
-                $contentpublicurl = $customparams['custom_currikistudiohost'] . $contentpublicurl;
-                $page = <<< EOD
-<html>
-<head>
-    <title>1EdTech LTI message</title>
-</head>
-<body>
-    <iframe style="border: none; overflow: scroll;" width="100%" height="100%" src="$contentpublicurl" allowfullscreen="true"></iframe>
-</body>
-</html>
-EOD;
-                echo $page;
-                exit;
+                Activity::is_public();
             }
             if (isset($_GET['tools'])) {
                 header('Content-type: text/html');
@@ -418,7 +374,7 @@ EOD;
         return $post;
     }
 
-    private function get_link_atts($post, $id)
+    public static function get_link_atts($post, $id)
     {
         $link_atts = array();
         $pattern = get_shortcode_regex(array(LTI_Platform::get_plugin_name()));
